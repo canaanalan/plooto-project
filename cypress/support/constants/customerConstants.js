d4938c9541ecbc4b9e26c7925d079cea2fcf0e4f
“canaanalan” <canaandevito7@gmail.com>
2023-01-24 10:13:35 -0800

    Update file name

--- cypress/support/constants/customerConstants.js
+++ cypress/support/constants/customerConstants.js
@@ -1,10 +1,10 @@
-//
+// Test login data
 export const TEST_EMAIL= "myTestEmail@plooto.com";
 export const TEST_PASSWORD = "testPassword123";
 
 // Percy snapshot names
 export const LOGIN = "Login Page";
-export const COMPANY_SELECTION = "'Company Select Page'";
+export const COMPANY_SELECTION = "Company Select Page";
 export const DASHBOARD = "Dashboard Page";
 export const PAYMENT_APPROVALS = "Payment Approvals Section";
 export const PENDING_PAYMENTS = "Pending Payments Section";
@@ -29,7 +29,8 @@ export const APPROVED_PAYMENT = "Approved Payment Page";
         //   cy.log(names)
         // })
 // But this wouldn't make sense in the real world, so I didn't fully implement it.
-export const companies  = [
+
+export const companyData  = [
     "Dream Home Improvements",
     "Record World",
     "Garden Guru",
